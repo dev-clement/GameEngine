e3081ac9558190e1c19ddb839fa2e2ea734aad23
Clément François <clement.francois.developpeur@gmail.com>
2023-12-30 15:12:27 +0100

    Add project template for Open part

--- PrimalEditor/GameProject/NewProject.cs
+++ PrimalEditor/GameProject/NewProject.cs
@@ -1,36 +1,87 @@
-﻿using System;
+﻿using PrimalEditor.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PrimalEditor.GameProject
 {
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string? ProjectType { get; set; }
+        [DataMember]
+        public string? ProjectFile { get; set; }
+        [DataMember]
+        public List<string>? Folders { get; set; }
+        public byte[]? Icon { get; set; }
+        public byte[]? Screenshot { get; set; }
+        public string? IconFilePath { get; set; }
+        public string? ScreenshotFilePath { get; set;}
+        public string? ProjectFilePath { get; set; }
+    }
     class NewProject : ViewModelBase
     {
-        private string _name = "New Project";
-        
-        public string Name
+        // TODO: get the path from the installation location
+        private readonly string _templatePath = @"D:\Primal\PrimalEditor\ProjectTemplates";
+        private string _projectName = "New Project";
+
+        public string ProjectName
         {
-            get => _name;
+            get => _projectName;
             set 
             { 
-                _name = value;
-                OnPropertyChanged(nameof(Name));
+                _projectName = value;
+                OnPropertyChanged(nameof(ProjectName));
             }
         }
 
-        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PrimalProject\";
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PrimalProject\";
 
-        public string Path
+        public string ProjectPath
         {
-            get => _path;
+            get => _projectPath;
             set
             {
-                _path = value;
-                OnPropertyChanged(nameof(Path));
+                _projectPath = value;
+                OnPropertyChanged(nameof(ProjectPath));
+            }
+        }
+
+        private ObservableCollection<ProjectTemplate> _projectTemplates = [];
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
+
+        public NewProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+            try
+            {
+                var templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+                foreach (var file in templateFiles)
+                {
+                    var template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Screenshot.png"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+
+                    _projectTemplates.Add(template);
+                }
+            }
+            catch(Exception ex) 
+            {
+                Debug.WriteLine(ex.Message);
+                // TODO: Log errors
             }
         }
     }
