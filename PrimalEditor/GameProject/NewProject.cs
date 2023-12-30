cedf2326979f333f8e5a2f75571ae078630409ab
Clément François <clement.francois.developpeur@gmail.com>
2023-12-30 10:41:26 +0100

    Add Common and GameObject to PrimalEditor

---
+++ PrimalEditor/GameProject/NewProject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimalEditor.GameProject
+{
+    class NewProject : ViewModelBase
+    {
+        private string _name = "New Project";
+        
+        public string Name
+        {
+            get => _name;
+            set 
+            { 
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PrimalProject\";
+
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                _path = value;
+                OnPropertyChanged(nameof(Path));
+            }
+        }
+    }
+}
